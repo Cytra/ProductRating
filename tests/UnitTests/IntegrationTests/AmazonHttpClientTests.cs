6fed41fe685235de192fd51f4f44b9f7dc89f963
Martynas Samuilovas <martynas.samuilovas@saldo.com>
2022-12-13 00:42:53 +0200

    added integration tests

---
+++ tests/UnitTests/IntegrationTests/AmazonHttpClientTests.cs
@@ -0,0 +1,37 @@
+﻿using Infrastructure.Scrapers;
+using Xunit;
+using System.Net;
+using FluentAssertions;
+
+namespace UnitTests.IntegrationTests;
+
+public class AmazonHttpClientTests
+{
+    [Fact]
+    public async Task SearchProduct()
+    {
+        var httpClient = GetHttpClient();
+        var sut = new AmazonHttpClient(httpClient);
+        var products = await sut.SearchProducts("Dell Laptop", 1);
+        products.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task GetProductByAsin()
+    {
+        var httpClient = GetHttpClient();
+        var sut = new AmazonHttpClient(httpClient);
+        var products = await sut.GetProductByAsin("B08S71Y7M7");
+        products.Should().NotBeNull();
+    }
+
+    private HttpClient GetHttpClient()
+    {
+        var httpClient = new HttpClient(new HttpClientHandler
+        {
+            AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+        });
+        return httpClient;
+
+    }
+}
